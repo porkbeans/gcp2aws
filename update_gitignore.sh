d91664861bb237860c554647a29041507558760d
Takahiro Mizuo <me@porkbeans.dev>
2022-12-20 21:55:06 +0900

    chore: Update .gitignore

--- update_gitignore.sh
+++ update_gitignore.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
 gibo dump JetBrains Go Terraform >.gitignore
-echo '.terraform.lock.hcl' >>.gitignore
-echo 'dist/' >>.gitignore
+{
+  # Ignore example Terraform lock file
+  echo '.terraform.lock.hcl'
+
+  # Ignore builds
+  echo 'dist/'
+  echo 'gcp2aws'
+} >>.gitignore
